14ba68448fbe83a404888965c79043203b9f9b9f
wslominski <slominskiw@g.cofc.edu>
2026-04-01 12:21:51 -0400

    updated-stick-and-mikey

--- root/components/RenderCharacter.tsx
+++ root/components/RenderCharacter.tsx
@@ -1,38 +1,37 @@
 import { Character } from "@/interfaces/interfaces";
-import { toggleFavorite } from "@/storage/storage";
+import { checkFavorite, toggleFavorite } from "@/storage/storage";
 import { useEffect, useState } from "react";
-import {
-  View,
-  Text,
-  FlatList,
-  Image,
-  TouchableOpacity,
-} from "react-native";
+import { View, Text, FlatList, Image, TouchableOpacity } from "react-native";
 
 export const RenderCharacter = ({ item }: { item: Character }) => {
-    const [characters, setCharacters] = useState<Character[]>([]);
-    const [fav, isFav] = useState<boolean | null>(null);
+  const [fav, setFav] = useState<boolean | null>(null);
 
-    
-    return (
-      <View style={{ flexDirection: "row", padding: 10 }}>
-        <Image
-          source={{ uri: item.image }}
-          style={{ width: 60, height: 60 }}
-        />
+  useEffect(() => {
+    const checkAndSetFavorite = async () => {
+      const favorite = await checkFavorite(item);
+      setFav(Boolean(favorite));
+    };
+    checkAndSetFavorite();
+  }, [item]);
 
-        <View style={{ flex: 1, marginLeft: 10 }}>
-          <Text>{item.name}</Text>
-          <Text>{item.species}</Text>
-        </View>
+  const handleToggleFavorite = async () => {
+    await toggleFavorite(item);
+    const favorite = await checkFavorite(item);
+    setFav(Boolean(favorite));
+  };
 
-        <TouchableOpacity
-          onPress={() => toggleFavorite(item)}
-        >
-          <Text style={{ fontSize: 24 }}>
-            {fav ? "❤️" : "🤍"}
-          </Text>
-        </TouchableOpacity>
+  return (
+    <View style={{ flexDirection: "row", padding: 10 }}>
+      <Image source={{ uri: item.image }} style={{ width: 60, height: 60 }} />
+
+      <View style={{ flex: 1, marginLeft: 10 }}>
+        <Text>{item.name}</Text>
+        <Text>{item.species}</Text>
       </View>
-    );
-  }
+
+      <TouchableOpacity onPress={() => handleToggleFavorite()}>
+        <Text style={{ fontSize: 24 }}>{fav ? "❤️" : "🤍"}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
--- root/storage/storage.ts
+++ root/storage/storage.ts
@@ -1,12 +1,60 @@
 // Create and export three functions that get favorites, update favorites, and toggle favorites using AsyncStorage.
 // Use the FAVORITES_KEY constant as the key for AsyncStorage
 
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { Character } from "@/interfaces/interfaces";
+import { FAVORITES_KEY } from "@/keys/keys";
 
 export const getFavorites = async () => {
+    try{
+        const favs =  await AsyncStorage.getItem(FAVORITES_KEY)
+        if (favs){
+            return JSON.parse(favs) as Character[]
+        } else{
+            return []
+        }
+    } catch (e) {
+        console.log(e)
+    }
     
 };
 
-export const updateFavorites = async (favorites: Character[]) => {};
+export const updateFavorites = async (favorites: Character[]) => {
+    try {
+        await AsyncStorage.setItem(FAVORITES_KEY, JSON.stringify(favorites))
+    } catch (e) {
+        console.log(e)
+    }
+};
 
-export const toggleFavorite = async (favorite: Character) => {};
+export const toggleFavorite = async (favorite: Character) => {
+    console.log("Toggling favorite:", favorite)
+    try {
+        const favs = await AsyncStorage.getItem(FAVORITES_KEY)
+        let favorites: Character[] = favs ? JSON.parse(favs) : []
+
+        if (await checkFavorite(favorite)) {
+            favorites = favorites.filter((c) => c.id !== favorite.id)
+        } else {
+            favorites.push(favorite)
+        }
+        
+        await updateFavorites(favorites)
+    } catch (e) {
+        console.log(e)
+    }
+};
+
+export const checkFavorite = async (character: Character) => {
+    try {
+        const favs = await AsyncStorage.getItem(FAVORITES_KEY)
+        if (favs){
+            const favChars = JSON.parse(favs) as Character []
+            return favChars.some((c) => c.id === character.id)
+        } else {
+            return false
+        } 
+    } catch (e){
+        console.log(e)
+    }
+}
